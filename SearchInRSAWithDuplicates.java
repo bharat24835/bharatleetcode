cf072e30539a8bddcd165180d0329f93d57b147c
Bharat <97047293+bharat24835@users.noreply.github.com>
2022-02-25 10:40:57 +0530

    Add files via upload

--- SearchInRSAWithDuplicates.java
+++ SearchInRSAWithDuplicates.java
@@ -7,22 +7,30 @@ public class SearchInRSAWithDuplicates {
         Scanner in = new Scanner(System.in);
         System.out.println("We are searching in RSA with duplicates:");
         System.out.println("It is difficult to find the pivit in duplicates");
-        int []a ={2,2,2,2,2,2,9,1};
+        int []a ={9};
         int pivit = PivitWithDuplicates(a);
-        if (pivit!=-1)
+        System.out.println("  Pivit is : " + pivit);
+        System.out.println(" Enter the target : ");
+        int target = in.nextInt();
+        int found =0;
+        if(pivit != -1)
         {
-            System.out.println("We find the pivit at index "+pivit+" and element is "+a[pivit]);
-            System.out.println("Number of rotation in RSA are : " + (pivit+1));
+            if(a[0] > target)
+                found = BS(a,target,pivit+1,a.length-1);
+            else
+                found = BS(a,target,0,pivit);
         }
         else
-            System.out.println("Array is not rotated:");
+            found = BS(a,target,0,a.length-1);
+
+        System.out.println(" We found the element at index : " + found);
 
     }
     public static int PivitWithDuplicates(int[]a)
     {
         int start=0;
         int end= a.length-1;
-        while(start<=end)
+        while(start <= end)
         {
             int mid = start + (end - start)/2;
             if( end > mid && a[mid] > a[mid+1])
@@ -39,20 +47,35 @@ public class SearchInRSAWithDuplicates {
                 //checking if start is pivit or not
                 // if it is pivit , it will return start
                 // otherwise remove start
-                if (a[start] > a[start+1])
+                if ( start < end  && a[start] > a[start+1])
                     return start;
                 start++;
                 // checking if end is pivit or not
-                if (a[end] < a[end-1])
+                if ( start < end && a[end] < a[end-1])
                     return (end-1);
                 end--;
             }
             // left side is sorted , pivit should be in right
             else if (a[start] < a[mid] || a[start] == a[mid] && a[mid] > a[end])
+                // [2,3,4,5,6,7,1] && [2,2,2,2,2,2,6]
                 start= mid+1;
             else
                 end = mid-1;
 
+        }
+        return -1;
+    }
+    public static int BS(int []a ,int target, int start , int end)
+    {
+        while(start <= end)
+        {
+            int mid = start + (end - start )/2;
+            if (a[mid] > target)
+                end = mid-1;
+            else if(a[mid] < target)
+                start = mid+1;
+            else
+                return mid;
         }
         return -1;
     }
